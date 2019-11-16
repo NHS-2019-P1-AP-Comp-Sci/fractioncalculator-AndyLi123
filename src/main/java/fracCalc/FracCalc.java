420dd8a0788a3e8c66d1568dc6d6e8fe329b9529
s-liandy <s-liandy@53S500038109.it.bsd405.org>
2019-11-15 16:00:30 -0800

    push

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -4,29 +4,66 @@
 
 package fracCalc;
 
+import java.util.*;
+
 public class FracCalc {
 
-    public static void main(String[] args)
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
+	public static void main(String[] args) {
+		String str = produceAnswer(userInput());
+		System.out.println(str);
+		// TODO: Read the input from the user and call produceAnswer with an equation
 
-    }
+	}
 
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
-        // TODO: Implement this function to produce the solution to the input
+	public static String userInput() {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Type equation below:");
+		String x = userInput.nextLine();
+		userInput.close();
+		return x;
+	}
 
-        return "";
-    }
+	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
+	// test your code
+	// This function takes a String 'input' and produces the result
+	//
+	// input is a fraction string that needs to be evaluated. For your program, this
+	// will be the user input.
+	// e.g. input ==> "1/2 + 3/4"
+	//
+	// The function should return the result of the fraction after it has been
+	// calculated
+	// e.g. return ==> "1_1/4"
+	public static String produceAnswer(String in) {
+		String i = in;
+		int space = i.indexOf(" ");
+		String str = "";
+		if (Character.isWhitespace(i.charAt(space + 2))) {
+			char operator = i.charAt(space + 1);
+			str = (i.substring(space + 3, i.length()));
+//			if (str.indexOf("/") != -1) {
+//				int frac = str.indexOf("/");
+//				double firstFraction = Double.parseDouble(i.substring(0, operator));
+//				double SecondFraction = Double.parseDouble(i.substring(operator + 1, str.length()));
+//				if (str.indexOf("_") != -1) {
+//					int dash = str.indexOf("_");
+//					char wholeNumber = str.charAt(dash - 1);
+//					double num = Double.parseDouble(str.substring(0, frac));
+//					double denom = Double.parseDouble(str.substring(frac + 1, str.length()));
+//					double finalValue = wholeNumber + num + denom;
+//				}
+//
+//			}
+		}
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
+		// "1/2 + 3/4"
+
+		// TODO: Implement this function to produce the solution to the input
+
+		return str;
+	}
+
+	// TODO: Fill in the space below with any helper methods that you think you will
+	// need
 
 }
